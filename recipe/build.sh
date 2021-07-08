c91d0fabbc4154fe6ae2db47c2b951354bd9fd09
Felipe Marques de Almeida <felipemarques89@gmail.com>
2021-07-07 21:13:44 -0300

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,8 +9,10 @@ conda update --all --yes
 
 # Copy main file
 mkdir -p $PREFIX/bin  || exit 1;
+mkdir -p $PREFIX/src  || exit 1;
 
 cp $RECIPE_DIR/bin/* $PREFIX/bin/
+cp $RECIPE_DIR/src/* $PREFIX/src/
 
 # Fix perl
 $PREFIX/bin/cpanm -i Statistics::Basic
